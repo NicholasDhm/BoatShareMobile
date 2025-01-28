ccb1711519d46a375aa20faf56f996f32ca9251e
nicolasmichielon <nicolasmichielon@gmail.com>
2025-01-28 19:18:39 -0300

    add styled-components for CalendarDay and CalendarHeader components

---
+++ src/components/calendar-day/styles.ts
@@ -0,0 +1,16 @@
+import styled from "styled-components/native";
+
+export const Container = styled.View`
+  height: 86px;
+  width: 64px;
+
+  border-width: 1px;
+  border-color: gray;
+
+  justify-content: center;
+  align-items: center;
+`;
+
+export const DayText = styled.Text`
+  font-size: 18px;
+`;
--- src/components/calendar-header/calendar-header.tsx
+++ src/components/calendar-header/calendar-header.tsx
@@ -1,55 +1,20 @@
-import { View, Text, StyleSheet, Pressable } from "react-native";
 import { ArrowLeft, ArrowRight } from "lucide-react-native";
+
+import { Container, TextContainer, Button, DateText } from "./styles";
 
 export function CalendarHeader() {
   return (
-    <View style={styles.container}>
-      <Pressable style={styles.buttonContainer}>
+    <Container>
+      <Button>
         <ArrowLeft />
-      </Pressable>
-      <View style={styles.textContainer}>
-        <Text style={styles.text}>2024</Text>
-        <Text style={styles.text}>March</Text>
-      </View>
-      <Pressable style={styles.buttonContainer}>
+      </Button>
+      <TextContainer>
+        <DateText>2024</DateText>
+        <DateText>March</DateText>
+      </TextContainer>
+      <Button>
         <ArrowRight />
-      </Pressable>
-    </View>
+      </Button>
+    </Container>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: "#a2d9eb",
-    paddingHorizontal: 16,
-    height: 64,
-    width: 468,
-
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-
-    borderRadius: 4,
-  },
-  buttonContainer: {
-    backgroundColor: "#d7e2e9",
-    height: 48,
-    width: 48,
-
-    justifyContent: "center",
-    alignItems: "center",
-
-    borderRadius: 4,
-    borderWidth: 1,
-    borderColor: "#bdbdbdff",
-  },
-  textContainer: {
-    flex: 1,
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "space-evenly",
-  },
-  text: {
-    fontSize: 20,
-  },
-});
