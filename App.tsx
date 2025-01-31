f592371eca3b5f4541697945dc9d9111568af6c1
NicholasDhm <nickderham10@gmail.com>
2025-01-31 02:35:09 +0000

    WIP Profile page

--- App.tsx
+++ App.tsx
@@ -5,11 +5,13 @@ import { StatusBar } from "expo-status-bar";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 import { Routes } from "./src/routes";
+import { Profile } from "./src/screens/profile";
 
 export default function App() {
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: "#e6f2ff" }}>
-      <Routes />
+      {/* <Routes /> */}
+      <Profile></Profile>
       <StatusBar style="auto" />
     </SafeAreaView>
   );
---
+++ src/screens/profile/styles.ts
@@ -0,0 +1,24 @@
+import styled from "styled-components/native";
+
+export const Container = styled.View`
+  background-color: #e6f2ff;
+  padding: 32px;
+  border-radius: 8px;
+  gap: 10px;
+`;
+
+export const DataContainer = styled.View`
+  background-color:rgb(192, 199, 206);
+
+  padding: 16px;
+  border-radius: 8px;
+  gap: 6px;
+`;  
+
+export const Title = styled.Text`
+  font-size: 24px;
+`;
+
+export const SubText = styled.Text`
+  font-size: 16px;
+`;
