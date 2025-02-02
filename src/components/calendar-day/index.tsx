4cccbcd124d90ff6ab70a76a2e77a14182f6213f
NicholasDhm <nickderham10@gmail.com>
2025-02-02 18:38:11 +0000

    Add function to display the type of the earliest reservation in CalendarDay

--- src/components/calendar-day/index.tsx
+++ src/components/calendar-day/index.tsx
@@ -2,11 +2,18 @@ import { CalendarDayProps } from "../../types/calendar-day";
 import { Container, DayText } from "./styles";
 
 export function CalendarDay({ day, reservations }: CalendarDayProps) {
+  function FirstReservation() {
+    return reservations.reduce((earliest, current) => {
+      return new Date(earliest.createdAtIsoDate) < new Date(current.createdAtIsoDate) ? earliest : current;
+    });
+  }
+
+
   return (
     <Container>
       <DayText>{day}</DayText>
       {reservations.length > 0 && (
-        <DayText>Reserved by {reservations.length} people</DayText>
+        <DayText>{FirstReservation().type}</DayText>
       )}
     </Container>
   );
