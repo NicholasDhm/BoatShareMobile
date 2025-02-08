a6974af7582f15bfd4a6f325464e145d083105f9
nicolasmichielon <nicolasmichielon@gmail.com>
2025-02-08 17:20:58 -0300

    Refactor Profile screen UI with improved boat and user information display

--- src/screens/profile/styles.ts
+++ src/screens/profile/styles.ts
@@ -1,22 +1,78 @@
 import { StyleSheet } from 'react-native';
+import { colors } from '../../themes/colors';
 
 export const styles = StyleSheet.create({
   container: {
-    backgroundColor: '#e6f2ff',
+
+    flex: 1,
     padding: 32,
     borderRadius: 8,
-    gap: 18,
+    gap: 20,
   },
+
   dataContainer: {
-    backgroundColor: 'rgb(192, 199, 206)',
-    padding: 16,
-    borderRadius: 8,
-    gap: 6,
+    padding: 15,
+    backgroundColor: colors.grayLight,
+    borderRadius: 10,
+    gap: 2,
+
+    shadowColor: "black",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
   },
+
   title: {
+    fontSize: 20,
+  },
+
+
+  pageTitle: {
     fontSize: 24,
+    fontWeight: "bold",
+    marginLeft: 10,
   },
   subText: {
     fontSize: 16,
+    color: colors.grayDark,
+  },
+
+
+
+  boatContainer: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  dropdownHeader: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  dropdownTitleContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  dropdownTitle: {
+    fontSize: 16,
+  },
+  row: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    marginBottom: 10,
+  },
+  boatRow: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 10,
+  },
+  boatName: {
+    marginInline: 16,
+    fontSize: 16,
+  },
+  userIcon: {
+    marginRight: 4,
   },
 });
+
