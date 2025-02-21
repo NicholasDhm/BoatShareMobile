985e8b590cb859df3024262e1fb12a0904e03612
nicolasmichielon <nicolasmichielon@gmail.com>
2025-02-21 17:17:51 -0300

    add animation to calendar header button

--- src/components/calendar-header/index.tsx
+++ src/components/calendar-header/index.tsx
@@ -1,6 +1,7 @@
 import { View, Text, Pressable } from "react-native";
 import { ArrowLeft, ArrowRight } from "lucide-react-native";
 import { styles } from "./styles";
+import Animated, { useAnimatedStyle, useSharedValue, withSpring } from "react-native-reanimated";
 
 type CalendarHeaderProps = {
   year: number;
@@ -19,18 +20,53 @@ export function CalendarHeader({
     month: "long",
   });
 
+  // Animation stuff
+  const scaleLeft = useSharedValue(1);
+  const scaleRight = useSharedValue(1);
+
+  const animatedButtonLeftStyle = useAnimatedStyle(() => {
+    return {
+      transform: [{ scale: scaleLeft.value }]
+    };
+  });
+
+  const animatedButtonRightStyle = useAnimatedStyle(() => {
+    return {
+      transform: [{ scale: scaleRight.value }]
+    };
+  });
+
+  function onPressInLeft() {
+    scaleLeft.value = withSpring(0.9);
+  }
+
+  function onPressOutLeft() {
+    scaleLeft.value = withSpring(1);
+  }
+
+  function onPressOutRight() {
+    scaleRight.value = withSpring(1);
+  }
+  function onPressInRight() {
+    scaleRight.value = withSpring(0.9);
+  }
+
   return (
     <View style={styles.container}>
-      <Pressable style={styles.button} onPress={onLeftPress}>
-        <ArrowLeft size={16} color={"black"} />
-      </Pressable>
+      <Animated.View style={[styles.button, animatedButtonLeftStyle]}>
+        <Pressable onPress={onLeftPress} onPressIn={onPressInLeft} onPressOut={onPressOutLeft} hitSlop={15}>
+          <ArrowLeft size={16} color={"black"} />
+        </Pressable>
+      </Animated.View>
       <View style={styles.textContainer}>
         <Text style={styles.dateText}>{year}</Text>
         <Text style={styles.dateText}>{monthText}</Text>
       </View>
-      <Pressable style={styles.button} onPress={onRightPress}>
-        <ArrowRight size={16} color={"black"} />
-      </Pressable>
+      <Animated.View style={[styles.button, animatedButtonRightStyle]}>
+        <Pressable onPress={onRightPress} onPressIn={onPressInRight} onPressOut={onPressOutRight} hitSlop={15}>
+          <ArrowRight size={16} color={"black"} />
+        </Pressable>
+      </Animated.View>
     </View>
   );
 }
